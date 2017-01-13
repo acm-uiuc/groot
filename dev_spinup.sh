9561191d20cd261e11ad83a4e8bd262554544de7
Sameet Sapra <sameetandpotatoes@gmail.com>
2017-01-13 09:31:31 -0600

    Update scripts

--- dev_spinup.sh
+++ dev_spinup.sh
@@ -2,101 +2,52 @@
 
 mkdir -p log/dev
 
-#########################################################################
-#																		#
-#																		#
-#			I N S T A L L   G R O O T   A P I   G A T E W A Y 		    #
-#																		#
-#																		#
-#########################################################################
-	
+# Groot API
 ./groot/build.sh 
 cp groot/build/groot build/groot
 
-#########################################################################
-#																		#
-#																		#
-#		I N S T A L L    G R O O T   U S E R S   S E R V I C E		    #
-#																		#
-#																		#
-#########################################################################
-
+# Groot Users Service
 cd groot-users-service
-
 npm install
-
 cd ..
 
-#########################################################################
-#																		#
-#																		#
-#		I N S T A L L   G R O O T   G R O U P S   S E R V I C E			#
-#																		#
-#																		#
-#########################################################################
-
+# Groot Groups Service
 cd groot-groups-service
-
 npm install
-
 cd ..
 
-#########################################################################
-#																		#
-#																		#
-#	  I N S T A L L   G R O O T   D E S K T O P   F R O N T E N D	    #
-#																		#
-#																		#
-#########################################################################
-
+# Groot Desktop Frontend
 cd groot-desktop-frontend
-
 npm install
-
 cd ..
 
+# Groot Events Service
+cd groot-events-service
+npm install
+cd ..
 
-#########################################################################
-#																		#
-#																		#
-#	I N S T A L L   G R O O T   R E C R U I T E R S   S E R V I C E		#
-#																		#
-#																		#
-#########################################################################
-
+# Groot Recruiters Service
 cd groot-recruiters-service
-
 bundle install
-
 cd ..
 
-#########################################################################
-#																		#
-#																		#
-#		I N S T A L L   G R O O T   Q U O T E S   S E R V I C E 		#
-#																		#
-#																		#
-#########################################################################
-
+# Groot Quotes Service
 cd groot-quotes-service
-
 bundle install
-
 cd ..
 
-#########################################################################
-#																		#
-#																		#
-#	 					 R U N    G R O O T	   							#
-#																		#
-#																		#
-#########################################################################
+# Groot Meme Service
+cd groot-meme-service
+pip install -r requirements.txt
+cd ..
 
-trap 'kill %1; kill %2; kill %3; kill %4; kill %5;' SIGINT
+trap 'kill %1; kill %2; kill %3; kill %4; kill %5; kill %6; kill %7;' SIGINT
 
 node groot-users-service/server.js  | tee log/dev/groot-users-service.log | sed -e 's/^/[groot-users-service] /' \
 & node groot-groups-service/server.js  | tee log/dev/groot-groups-service.log | sed -e 's/^/[groot-groups-service] /' \
 & npm --prefix ./groot-desktop-frontend start  | tee log/dev/groot-desktop-frontend.log | sed -e 's/^/[groot-desktop-frontend] /' \
+& node groot-events-service/server.js  | tee log/dev/groot-events-service.log | sed -e 's/^/[groot-events-service] /' \
 & ruby groot-recruiters-service/app.rb | tee log/dev/groot-recruiters-service.log | sed -e 's/^/[groot-recruiters-service] /' \
 & ruby groot-quotes-service/app.rb | tee log/dev/groot-quotes-service.log | sed -e 's/^/[groot-quotes-service] /' \
+& python groot-meme-service/groot_meme_service/app.py | tee log/dev/groot-meme-service.log | sed -e 's/^/[groot-meme-service] /' \
 & ./build/groot -u | tee log/dev/groot.log | sed -e 's/^/[groot] /'
