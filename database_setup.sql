ac2c7e12ff70fb7d792159f93f50ec1ae00bbc47
Benjamin Congdon <bencongdon96@gmail.com>
2017-03-31 09:08:56 -0500

    MySQL config updates

--- database_setup.sql
+++ database_setup.sql
@@ -1,6 +1,9 @@
 CREATE DATABASE IF NOT EXISTS groot_caffeine_service_dev;
 CREATE DATABASE IF NOT EXISTS groot_credits_service;
 CREATE DATABASE IF NOT EXISTS groot_meme_service;
+CREATE DATABASE IF NOT EXISTS groot_merch_service_dev;
 CREATE DATABASE IF NOT EXISTS groot_quote_service_dev;
 CREATE DATABASE IF NOT EXISTS groot_recruiter_service_dev;
 CREATE DATABASE IF NOT EXISTS groot_user_service_dev;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
+FLUSH PRIVILEGES;
